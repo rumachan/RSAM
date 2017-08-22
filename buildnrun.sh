5e86bb40c1ceb88fa13014cf1e39f00a428476df
Yannik Behr <behrya@vulkan.gns.cri.nz>
2017-08-22 15:09:59 +1200

    add script to build and run docker image

---
+++ buildnrun.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#########################################
+# Build and run docker image            #
+# 08/17 Y. Behr <y.behr@gns.cri.nz>     #
+#########################################
+
+CONTAINERONLY=false
+IMAGEONLY=false
+
+function usage(){
+cat <<EOF
+Usage: $0 [Options]
+
+Build and run docker image. 
+
+Options:
+    -h              Show this message.
+    -r              Only rebuild and rerun container without 
+                    rebuilding the images
+    -i              Only rebuild image without running the container.
+EOF
+}
+
+# Processing command line options
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -r) CONTAINERONLY=true;;
+        -i) IMAGEONLY=true;;
+        -h) usage; exit 0;;
+        -*) usage; exit 1;;
+        *) break;;
+esac
+shift
+done
+
+if [ "${CONTAINERONLY}" == "false" ]; then
+    docker rmi yadabe/rsam
+    docker build --no-cache=true -t yadabe/rsam .
+fi
+
+if [ "${IMAGEONLY}" == "false" ] ;then
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam.sh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam_plot.csh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam_day.sh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam_plot_day.csh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam ascii_daily.sh
+
+
+fi
+
+
