e84a6c7b0d55bf0b83f1d1b8576b6e1b76e305e1
Yannik Behr <yannik@yanmail.de>
2017-12-14 11:30:59 +1300

    updating build script

--- buildnrun.sh
+++ buildnrun.sh
@@ -5,8 +5,8 @@
 # 08/17 Y. Behr <y.behr@gns.cri.nz>     #
 #########################################
 
-CONTAINERONLY=false
-IMAGEONLY=false
+RUNONLY=false
+BUILDONLY=false
 
 function usage(){
 cat <<EOF
@@ -18,15 +18,19 @@ Options:
     -h              Show this message.
     -r              Only run image without rebuilding it.
     -b              Only rebuild image without running it.
+    -t              Assign a tag to the docker image (default: latest).
 EOF
 }
+
+TAG="latest"
 
 # Processing command line options
 while [ $# -gt 0 ]
 do
     case "$1" in
-        -r) CONTAINERONLY=true;;
-        -i) IMAGEONLY=true;;
+        -r) RUNONLY=true;;
+        -b) BUILDONLY=true;;
+        -t) TAG=$2;shift;;
         -h) usage; exit 0;;
         -*) usage; exit 1;;
         *) break;;
@@ -34,19 +38,17 @@ esac
 shift
 done
 
-if [ "${CONTAINERONLY}" == "false" ]; then
-    docker rmi yadabe/rsam
-    docker build --no-cache=true -t yadabe/rsam .
+if [ "${RUNONLY}" == "false" ]; then
+    docker rmi yadabe/rsam:$TAG
+    docker build --no-cache=true -t yadabe/rsam:$TAG .
 fi
 
-if [ "${IMAGEONLY}" == "false" ] ;then
-    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam.sh
-    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam_plot.csh
-    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam_day.sh
-    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam my_rsam_plot_day.csh
-    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam ascii_daily.sh
-
-
+if [ "${BUILDONLY}" == "false" ] ;then
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam:$TAG my_rsam.sh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam:$TAG my_rsam_plot.csh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam:$TAG my_rsam_day.sh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam:$TAG my_rsam_plot_day.csh
+    docker run --rm -v html:/output -v rsam:/workdir yadabe/rsam:$TAG ascii_daily.sh
 fi
 
 
