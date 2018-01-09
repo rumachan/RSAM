1c1be431c4e749f967d25c5748030c34a3e60fbd
Yannik Behr <yannik@yanmail.de>
2018-01-10 09:21:32 +1300

    adding 2018 files

--- ascii_daily.sh
+++ ascii_daily.sh
@@ -22,7 +22,8 @@ rsamfile2ascii.py ./workdir/MAVZ.NZ/2014.MAVZ.10-HHZ.NZ.bp_1.00-4.00.rsam ./work
 rsamfile2ascii.py ./workdir/MAVZ.NZ/2015.MAVZ.10-HHZ.NZ.bp_1.00-4.00.rsam ./workdir/my_rsam/2015.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc
 rsamfile2ascii.py ./workdir/MAVZ.NZ/2016.MAVZ.10-HHZ.NZ.bp_1.00-4.00.rsam ./workdir/my_rsam/2016.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc
 rsamfile2ascii.py ./workdir/MAVZ.NZ/2017.MAVZ.10-HHZ.NZ.bp_1.00-4.00.rsam ./workdir/my_rsam/2017.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc
+rsamfile2ascii.py ./workdir/MAVZ.NZ/2018.MAVZ.10-HHZ.NZ.bp_1.00-4.00.rsam ./workdir/my_rsam/2018.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc
                   
-cat ./workdir/my_rsam/2013.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2014.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2015.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2016.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2017.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc > ./workdir/my_rsam/MAVZ.asc
+cat ./workdir/my_rsam/2013.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2014.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2015.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2016.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2017.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc ./workdir/my_rsam/2018.MAVZ.10-HHZ.NZ.bp_1.00-4.00.asc > ./workdir/my_rsam/MAVZ.asc
 
 sed -i '/TIME/d' ./workdir/my_rsam/MAVZ.asc
